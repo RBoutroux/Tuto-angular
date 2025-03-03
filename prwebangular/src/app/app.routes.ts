51b01ce0fd9bf4c7cfb6cc3df3ff1f0790dff7bf
RBoutroux <remibtrx@gmail.com>
2025-03-03 16:13:28 +0100

    [feat] login route

--- prwebangular/src/app/app.routes.ts
+++ prwebangular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AuthenticateComponent } from './authenticate/authenticate.component';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: AuthenticateComponent}
+];
